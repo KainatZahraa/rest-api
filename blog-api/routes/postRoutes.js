2b715869a3290c70d31d818475ce298c876268b5
Kainat Zahra <kainatzahra1012@gmail.com>
2026-05-09 12:59:51 +0500

    add routes - user, post, comment routes

--- blog-api/routes/postRoutes.js
+++ blog-api/routes/postRoutes.js
@@ -1,4 +1,26 @@
 const express = require('express');
 const router = express.Router();
+const {
+  createPost,
+  getAllPosts,
+  getPostsByTag,
+  getPostById,
+  updatePost,
+  deletePost,
+} = require('../controllers/postController');
+const { addComment, getCommentsByPost } = require('../controllers/commentController');
+
+
+router.get('/tag/:tag', getPostsByTag);
+
+router.post('/', createPost);
+router.get('/', getAllPosts);
+router.get('/:id', getPostById);
+router.put('/:id', updatePost);
+router.delete('/:id', deletePost);
+
+// Comment routes under posts
+router.post('/:postId/comments', addComment);
+router.get('/:postId/comments', getCommentsByPost);
 
 module.exports = router;
